40977b837a2a24a08d58dd2b25b449ea740105de
SantiCalvo65 <santiagocalvo65@gmail.com>
2025-08-12 18:03:23 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
   base: "/proyecto-html-con-estilos/", // Usa rutas relativas en los enlaces generados en el HTML
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "quienes_somos.html"),
-        cursos: resolve(__dirname, "Cursos.html"),
-        curso_1: resolve(__dirname, "Curso_1.html"),
-        curso_2: resolve(__dirname, "Curso_2.html"),
-        Blog: resolve(__dirname, "Noticias.html"),
-        Blog_1: resolve(__dirname, "Noticias_1.html"),
-        Blog_2: resolve(__dirname, "Noticias_2.html"),
-        Registro: resolve(__dirname, "Registro.html"),
-        Login: resolve(__dirname, "Login.html"),
-        Contact: resolve(__dirname, "Contacto.html"),
+        main: resolve(__dirname, "./index.html"),
+        about: resolve(__dirname, "./quienes_somos.html"),
+        cursos: resolve(__dirname, "./Cursos.html"),
+        curso_1: resolve(__dirname, "./cursos/Curso_1.html"),
+        curso_2: resolve(__dirname, "./cursos/Curso_2.html"),
+        Blog: resolve(__dirname, "./Noticias.html"),
+        Blog_1: resolve(__dirname, "./noticias/Noticias_1.html"),
+        Blog_2: resolve(__dirname, "./noticias/Noticias_2.html"),
+        Registro: resolve(__dirname, "./Registro.html"),
+        Login: resolve(__dirname, "./Login.html"),
+        Contact: resolve(__dirname, "./Contacto.html"),
 
       },
     },
